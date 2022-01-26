cc3ac40bc8551338a5be2ed93dc01664674708ec
Chutiphong Charoenchao <chutiphongcharoenchao@Chutiphongs-MacBook-Pro.local>
2022-01-26 14:10:56 +0700

    :zap: setheader

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,7 +1,25 @@
-import Vue from 'vue';
-import Vuetify from 'vuetify/lib/framework';
+import Vue from "vue";
+import Vuetify from "vuetify/lib/framework";
 
 Vue.use(Vuetify);
 
 export default new Vuetify({
+  theme: {
+    themes: {
+      light: {
+        primary: "#031B26", // #E53935
+        secondary: "#04293A", // #FFCDD2
+        accent: "#064663", // #3F51B5
+        textw: "#eee",
+        textb: "#333",
+      },
+      dark: {
+        primary: "#031B26", // #E53935
+        secondary: "#04293A", // #FFCDD2
+        accent: "#064663", // #3F51B5
+        textw: "#eee",
+        textb: "#333",
+      },
+    },
+  },
 });
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,5 @@
 <template>
-  <hello-world />
+  <v-container grid-list-xs></v-container>
 </template>
 
-<script>
-  import HelloWorld from '../components/HelloWorld'
 
-  export default {
-    name: 'Home',
-
-    components: {
-      HelloWorld,
-    },
-  }
-</script>
